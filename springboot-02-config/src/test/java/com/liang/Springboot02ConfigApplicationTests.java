61f7fbd566b89b91104824310b01fa1a4b642159
LiangYang666 <mail.liangyang@gmail.com>
2021-09-08 17:11:19 +0800

    测试打印@Autowired装配的dog对象

--- springboot-02-config/src/test/java/com/liang/Springboot02ConfigApplicationTests.java
+++ springboot-02-config/src/test/java/com/liang/Springboot02ConfigApplicationTests.java
@@ -1,13 +1,21 @@
 package com.liang;
 
+import com.liang.pojo.Dog;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Springboot02ConfigApplicationTests {
+    @Autowired
+//    @Qualifier
+    private Dog dog;
 
     @Test
     void contextLoads() {
+        System.out.println(dog);
+
     }
 
 }
