c3bdb2dc24de3cc0d22bbc2f57457672a469c0ab
LiangYang666 <mail.liangyang@gmail.com>
2021-09-11 19:05:34 +0800

    删减配置

--- springboot-02-config/src/test/java/com/liang/Springboot02ConfigApplicationTests.java
+++ springboot-02-config/src/test/java/com/liang/Springboot02ConfigApplicationTests.java
@@ -1,7 +1,5 @@
 package com.liang;
 
-import com.liang.pojo.Dog;
-import com.liang.pojo.Person;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -9,14 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Springboot02ConfigApplicationTests {
-    @Autowired
-//    @Qualifier
-    private Person person;
 
     @Test
     void contextLoads() {
-        System.out.println(person);
-
     }
-
 }
