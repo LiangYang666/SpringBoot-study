06b1c3a4c6ca6c92b2ecc0559bce33db3fb725d2
LiangYang666 <mail.liangyang@gmail.com>
2021-09-13 15:13:54 +0800

    扩展SpringMvc官方建议方法实现WebMvcConfigurer接口-例子-创建视图跳转

--- springboot-03-web/src/main/java/com/liang/config/MyMvcConfig.java
+++ springboot-03-web/src/main/java/com/liang/config/MyMvcConfig.java
@@ -1,29 +1,16 @@
 package com.liang.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.servlet.View;
-import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Locale;
-
+//如果我们要扩展SpringMvc 官方建议我们这样去做
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer {
 
-    //ViewResolver实现了视图解析器接口的类
-    @Bean
-    public MyViewResolver myViewResolver(){
-        return new MyViewResolver();
-    }
-
-    //自定义一个自己的视图解析器
-    public static class MyViewResolver implements ViewResolver{
-        @Override
-        public View resolveViewName(String s, Locale locale) throws Exception {
-            return null;
-        }
+    // 试图跳转
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/liang").setViewName("test");
     }
 }
