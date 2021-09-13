897068b0babf813e459f8c971e2b79aa6aead4d4
LiangYang666 <mail.liangyang@gmail.com>
2021-09-13 13:02:44 +0800

    thymelef 取出设置的数据并显示 遍历列表显示

--- springboot-03-web/src/main/java/com/liang/controller/IndexController.java
+++ springboot-03-web/src/main/java/com/liang/controller/IndexController.java
@@ -1,12 +1,17 @@
 package com.liang.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
 
 @Controller
 public class IndexController {
     @RequestMapping("/test")
-    public String test(){
+    public String test(Model model){
+        model.addAttribute("msg", "<h2>Hello, thymeleaf</h2>");
+        model.addAttribute("users", Arrays.asList("1", "2"));
         return "test";
     }
 
