a1574906735f0bd234af047269baec3508a28b04
LiangYang666 <mail.liangyang@gmail.com>
2021-09-15 19:05:46 +0800

    显示首页

--- springboot-03-web/src/main/java/com/liang/config/MyMvcConfig.java
+++ springboot-03-web/src/main/java/com/liang/config/MyMvcConfig.java
@@ -8,10 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //如果我们要扩展SpringMvc 官方建议我们这样去做
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer {
-
     // 试图跳转
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/liang").setViewName("test");
+//        registry.addViewController("/liang").setViewName("test");
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index.html").setViewName("index");
     }
 }
