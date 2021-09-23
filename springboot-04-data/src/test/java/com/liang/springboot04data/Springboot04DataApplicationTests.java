e956bdeaa1177d4730626f8e25434e4123ae712c
LiangYang666 <mail.liangyang@gmail.com>
2021-09-23 23:25:18 +0800

    打印默认数据源

--- springboot-04-data/src/test/java/com/liang/springboot04data/Springboot04DataApplicationTests.java
+++ springboot-04-data/src/test/java/com/liang/springboot04data/Springboot04DataApplicationTests.java
@@ -1,13 +1,21 @@
 package com.liang.springboot04data;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
 
 @SpringBootTest
 class Springboot04DataApplicationTests {
+    @Autowired
+    DataSource dataSource;
 
     @Test
     void contextLoads() {
+        System.out.println("=====>>");
+        //查看默认数据源 class com.zaxxer.hikari.HikariDataSource
+        System.out.println(dataSource.getClass());
     }
 
 }
