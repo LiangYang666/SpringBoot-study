b920cf31f944023fe276bc4a32db09cca08f5918
LiangYang666 <mail.liangyang@gmail.com>
2021-09-23 15:29:18 +0800

    first

---
+++ springboot-04-data/src/test/java/com/liang/springboot04data/Springboot04DataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liang.springboot04data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot04DataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
