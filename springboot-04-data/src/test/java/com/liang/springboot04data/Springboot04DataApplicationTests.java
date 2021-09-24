25dc41c09e7b4efa4f8217329093ff71e1844baa
LiangYang666 <mail.liangyang@gmail.com>
2021-09-24 09:15:08 +0800

    打印默认数据源和默认连接

--- springboot-04-data/src/test/java/com/liang/springboot04data/Springboot04DataApplicationTests.java
+++ springboot-04-data/src/test/java/com/liang/springboot04data/Springboot04DataApplicationTests.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootTest
 class Springboot04DataApplicationTests {
@@ -12,10 +14,12 @@ class Springboot04DataApplicationTests {
     DataSource dataSource;
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws SQLException {
         System.out.println("=====>>");
         //查看默认数据源 class com.zaxxer.hikari.HikariDataSource
         System.out.println(dataSource.getClass());
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);     //HikariProxyConnection@163160871 wrapping com.mysql.cj.jdbc.ConnectionImpl@75a0c890
     }
 
 }
