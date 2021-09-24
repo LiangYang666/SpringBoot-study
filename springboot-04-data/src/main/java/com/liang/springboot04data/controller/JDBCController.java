bfa3b9ab0d3ab911453d742641082b7bec0e273c
LiangYang666 <mail.liangyang@gmail.com>
2021-09-24 17:18:35 +0800

    JdbcTemplate插入一个数据并使用ModelAndView转发回查询页面

--- springboot-04-data/src/main/java/com/liang/springboot04data/controller/JDBCController.java
+++ springboot-04-data/src/main/java/com/liang/springboot04data/controller/JDBCController.java
@@ -3,8 +3,12 @@ package com.liang.springboot04data.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -18,4 +22,12 @@ public class JDBCController {
         List<Map<String, Object>> listMap = jdbcTemplate.queryForList(sql);
         return listMap.toString();
     }
+    @GetMapping("/add")
+    public ModelAndView addUser(){
+        String sql = "insert into mybatis.user values (6, 'test6', '123543')";
+        jdbcTemplate.update(sql);
+        return new ModelAndView("redirect:/querytest");
+    }
+
+
 }
