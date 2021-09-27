3b875e6aeddfd5742488fa9e80bbca98dce89242
LiangYang666 <mail.liangyang@gmail.com>
2021-09-27 11:08:44 +0800

    配置druid的监控器 此部分待看视频学习

--- springboot-04-data/src/main/java/com/liang/springboot04data/config/DruidConfig.java
+++ springboot-04-data/src/main/java/com/liang/springboot04data/config/DruidConfig.java
@@ -1,12 +1,19 @@
 package com.liang.springboot04data.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class DruidConfig {
@@ -16,4 +23,31 @@ public class DruidConfig {
         return new DruidDataSource();
     }
 
+    @Bean
+    public ServletRegistrationBean statViewServlet(){
+        ServletRegistrationBean bean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+        HashMap<String, String> initParams = new HashMap<>();
+        initParams.put("loginUsername", "admin");
+        initParams.put("loginPassword", "123456");
+        initParams.put("allow", "");
+
+        bean.setInitParameters(initParams);
+
+        return bean;
+
+    }
+
+    public FilterRegistrationBean webStatFilter(){
+        FilterRegistrationBean bean = new FilterRegistrationBean();
+        bean.setFilter(new WebStatFilter());
+
+        //exclusions：设置哪些请求进行过滤排除掉，从而不进行统计
+        Map<String, String> initParams = new HashMap<>();
+        initParams.put("exclusions", "*.js,*.css,/druid/*,/jdbc/*");
+        bean.setInitParameters(initParams);
+
+        //"/*" 表示过滤所有请求
+        bean.setUrlPatterns(Arrays.asList("/*"));
+        return bean;
+    }
 }
