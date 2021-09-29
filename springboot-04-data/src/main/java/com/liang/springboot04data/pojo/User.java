46655387f2ca12a20f01e68cb58903da1cb99b7e
LiangYang666 <mail.liangyang@gmail.com>
2021-09-30 00:23:24 +0800

    实体类

---
+++ springboot-04-data/src/main/java/com/liang/springboot04data/pojo/User.java
@@ -0,0 +1,16 @@
+package com.liang.springboot04data.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class User {
+    private int id;
+    private String name;
+    private String pwd;
+}
