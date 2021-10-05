2cd8105ac592f1c977655bd5a5f6787efd795bc0
LiangYang666 <mail.liangyang@gmail.com>
2021-10-05 21:42:40 +0800

    添加ToString

--- springboot-03-web/src/main/java/com/liang/pojo/Department.java
+++ springboot-03-web/src/main/java/com/liang/pojo/Department.java
@@ -3,10 +3,12 @@ package com.liang.pojo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Department {
     private Integer id;
     private String department;
--- springboot-03-web/src/main/java/com/liang/pojo/Employee.java
+++ springboot-03-web/src/main/java/com/liang/pojo/Employee.java
@@ -3,12 +3,14 @@ package com.liang.pojo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.Date;
 
 
 @Data
 @NoArgsConstructor
+@ToString
 public class Employee {
     private Integer id;
     private String lastName;
