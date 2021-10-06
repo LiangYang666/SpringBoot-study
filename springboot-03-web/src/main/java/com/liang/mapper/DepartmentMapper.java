ea47a9e6113e6712de9c52d3e824dfb9804f8e40
LiangYang666 <mail.liangyang@gmail.com>
2021-10-06 10:09:00 +0800

    @Repository 显式地定义生成一个spring的bean，其实也可以不加

--- springboot-03-web/src/main/java/com/liang/mapper/DepartmentMapper.java
+++ springboot-03-web/src/main/java/com/liang/mapper/DepartmentMapper.java
@@ -4,10 +4,12 @@ import com.liang.pojo.Department;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Mapper
+@Repository
 public interface DepartmentMapper {
     List<Department> getAllDepartments();
 }
