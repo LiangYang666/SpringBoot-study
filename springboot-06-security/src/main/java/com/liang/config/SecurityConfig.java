baeafd88f9008d0b2db76fef5a547ab66ee37f8b
LiangYang666 <mail.liangyang@gmail.com>
2021-10-08 16:54:13 +0800

    指定登陆跳转的login页面

--- springboot-06-security/src/main/java/com/liang/config/SecurityConfig.java
+++ springboot-06-security/src/main/java/com/liang/config/SecurityConfig.java
@@ -15,10 +15,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers("/").permitAll()
                 .antMatchers("/level1/*").hasRole("vip1")
                 .antMatchers("/level2/*").hasRole("vip2")
-                .antMatchers("/level3/*").hasRole("vip3")
-                .and().formLogin();     //开启跳转login界面
-        http.logout().logoutSuccessUrl("/");      //  开启一个注销功能
-        http.rememberMe();      //开启记住我选项 若不退出 将登陆信息保存至cookie 默认保存两周
+                .antMatchers("/level3/*").hasRole("vip3");
+
+        http.formLogin()                   //开启跳转login界面
+                .loginPage("/toLogin")    // 指定请求登陆网址时的请求地址 如不指定默认为spring自带的/login 指定后为自己controller写的/toLogin
+                .loginProcessingUrl("/usr/login")   // 登陆信息成功后，表单提交的地址，地址为自己写表单时定义的 spring配置这个进行信息校验
+                .usernameParameter("username")      //登陆表单中的username字段名称 验证的话将由spring处理
+                .passwordParameter("password");     //登陆表单中的password字段名称 验证的话将由spring处理
+
+        http.logout().logoutUrl("/logout").logoutSuccessUrl("/");      //  开启一个注销功能
+
+        http.rememberMe()   //开启记住我选项 若不退出 将登陆信息保存至cookie 默认保存两周
+                .rememberMeParameter("remember");   // 配置记住我的参数 在表单中定义字段名称
     }
     //认证
     @Override
