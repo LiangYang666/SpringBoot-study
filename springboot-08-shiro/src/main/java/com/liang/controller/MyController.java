e006d9666c4581cea7a7d4ad93ec8bba0d2284f6
LiangYang666 <mail.liangyang@gmail.com>
2021-10-30 09:46:17 +0800

    添加登录和权限验证有关的controller

--- springboot-08-shiro/src/main/java/com/liang/controller/MyController.java
+++ springboot-08-shiro/src/main/java/com/liang/controller/MyController.java
@@ -1,8 +1,15 @@
 package com.liang.controller;
 
+import org.apache.catalina.security.SecurityUtil;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MyController {
@@ -19,4 +26,31 @@ public class MyController {
     public String toUpdate(){
         return "user/update";
     }
+    @RequestMapping("/toLogin")
+    public String toLogin(){
+        return "/login";
+    }
+    @RequestMapping("/login")
+    public String login(String username, String password, Model model){
+        Subject user = SecurityUtils.getSubject();
+
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+        try{
+            user.login(token);
+            model.addAttribute("msg", "成功登录");
+            return "index";
+        } catch (UnknownAccountException e){
+            model.addAttribute("msg", "用户名错误");
+            return "login";
+        } catch (IncorrectCredentialsException ice){
+            model.addAttribute("msg", "用户密码错误");
+            return "login";
+        }
+    }
+    @RequestMapping("/noAuth")
+    @ResponseBody
+    public String noAuth(){
+        return "未授权，无法访问";
+    }
+
 }
