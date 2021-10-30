7c36403c100fd6e0648c63cc7c376f4debdc00fa
LiangYang666 <mail.liangyang@gmail.com>
2021-10-30 09:54:19 +0800

    添加shiro的内置过滤器

--- springboot-08-shiro/src/main/java/com/liang/config/ShiroConfig.java
+++ springboot-08-shiro/src/main/java/com/liang/config/ShiroConfig.java
@@ -1,11 +1,15 @@
 package com.liang.config;
 
+import org.apache.commons.collections.map.LinkedMap;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.apache.shiro.web.mgt.WebSecurityManager;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Configuration
 public class ShiroConfig {
@@ -15,6 +19,21 @@ public class ShiroConfig {
     ){
         ShiroFilterFactoryBean bean = new ShiroFilterFactoryBean();
         bean.setSecurityManager(defaultWebSecurityManager);
+        // 添加shiro的内置过滤器
+        /*
+            anon: 无需认证就可以登录
+            authc:必须认证才能登录
+            user: 必须拥有“记住我”这个功能
+            perms:拥有对某个资源的权限才能访问
+            role:拥有某个角色才能访问
+         */
+        Map<String, String> filterMap = new LinkedHashMap<>();
+        filterMap.put("/user/add", "perms[user:add]");
+        filterMap.put("/user/update", "perms[user:update]");
+//        filterMap.put("/user/*", "authc");
+
+        bean.setFilterChainDefinitionMap(filterMap);
+        bean.setLoginUrl("/toLogin");
         return bean;
     }
 
