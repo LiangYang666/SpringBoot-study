fc2cdae6cd9d7ed7a73402b2579ae3566ee08892
LiangYang666 <mail.liangyang@gmail.com>
2021-11-04 00:09:45 +0800

    实现service

---
+++ springboot-08-shiro/src/main/java/com/liang/service/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package com.liang.service;
+
+import com.liang.mapper.UserMapper;
+import com.liang.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+    @Autowired
+    UserMapper userMapper;
+    @Override
+    public User queryUserByName(String name) {
+        return userMapper.queryUserByName(name);
+    }
+}
