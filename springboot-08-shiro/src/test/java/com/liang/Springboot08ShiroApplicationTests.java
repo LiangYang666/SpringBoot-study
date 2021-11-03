bae16bea674817f4accba7f4992ff26b5641c423
LiangYang666 <mail.liangyang@gmail.com>
2021-11-04 00:10:10 +0800

    测试是否能够从数据库中查询出来

--- springboot-08-shiro/src/test/java/com/liang/Springboot08ShiroApplicationTests.java
+++ springboot-08-shiro/src/test/java/com/liang/Springboot08ShiroApplicationTests.java
@@ -1,13 +1,18 @@
 package com.liang;
 
+import com.liang.service.UserService;
+import com.liang.service.UserServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Springboot08ShiroApplicationTests {
-
+    @Autowired
+    UserServiceImpl userService;
     @Test
     void contextLoads() {
+        System.out.println(userService.queryUserByName("zs"));
     }
 
 }
