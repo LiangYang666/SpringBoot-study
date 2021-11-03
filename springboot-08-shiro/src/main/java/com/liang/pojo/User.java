16f1308c8834c0e5308ef283ab17c0c098268795
LiangYang666 <mail.liangyang@gmail.com>
2021-11-04 00:07:51 +0800

    写实体类用 对应数据库

---
+++ springboot-08-shiro/src/main/java/com/liang/pojo/User.java
@@ -0,0 +1,17 @@
+package com.liang.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class User {
+    private int id;
+    private String name;
+    private String pwd;
+
+}
