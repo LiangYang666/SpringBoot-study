d2f7398b88c063e73725832d325eabee5dd1f9ab
LiangYang666 <mail.liangyang@gmail.com>
2021-11-05 01:08:03 +0800

    实现通过查询数据库 获取当前登陆用户的权限 进行相关拦截

--- springboot-08-shiro/src/main/java/com/liang/config/UserRealm.java
+++ springboot-08-shiro/src/main/java/com/liang/config/UserRealm.java
@@ -3,11 +3,13 @@ package com.liang.config;
 import com.liang.pojo.User;
 import com.liang.service.UserService;
 import com.liang.service.UserServiceImpl;
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserRealm extends AuthorizingRealm {
@@ -18,7 +20,12 @@ public class UserRealm extends AuthorizingRealm {
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
         System.out.println("==>执行了 1 doGetAuthorizationInfo");
         SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
-        info.addStringPermission("user:add");
+
+        Subject subject = SecurityUtils.getSubject();   //拿到当前登陆的对象
+        User user = (User) subject.getPrincipal();  //拿到user对象
+
+        info.addStringPermission(user.getPerms());  //设置当前用户的权限
+
         return info;
     }
 
@@ -31,6 +38,6 @@ public class UserRealm extends AuthorizingRealm {
             return null;
         }
         //密码认证 可以加密 md5 MD5盐值加密
-        return new SimpleAuthenticationInfo("", user.getPwd(), "");
+        return new SimpleAuthenticationInfo(user, user.getPwd(), "");   //存入user 方便在subject中取
     }
 }
