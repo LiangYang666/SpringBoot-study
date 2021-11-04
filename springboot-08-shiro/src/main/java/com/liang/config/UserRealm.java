4677b764746eae8e25ffdc0130a6c8407166e9b6
LiangYang666 <mail.liangyang@gmail.com>
2021-11-04 23:24:13 +0800

    自己实现从数据库查询到账号密码进行登陆检验

--- springboot-08-shiro/src/main/java/com/liang/config/UserRealm.java
+++ springboot-08-shiro/src/main/java/com/liang/config/UserRealm.java
@@ -1,11 +1,18 @@
 package com.liang.config;
 
+import com.liang.pojo.User;
+import com.liang.service.UserService;
+import com.liang.service.UserServiceImpl;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserRealm extends AuthorizingRealm {
+    @Autowired
+    UserService userService;
+
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
         System.out.println("==>执行了 1 doGetAuthorizationInfo");
@@ -15,12 +22,11 @@ public class UserRealm extends AuthorizingRealm {
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
         System.out.println("==>执行了 2 doGetAuthorizationInfo");
-        String name = "admin";
-        String password = "123456";
         UsernamePasswordToken userToken = (UsernamePasswordToken) authenticationToken;
-        if (!userToken.getUsername().equals(name)){
+        User user = userService.queryUserByName(userToken.getUsername());
+        if(user == null){
             return null;
         }
-        return new SimpleAuthenticationInfo("", password, "");
+        return new SimpleAuthenticationInfo("", user.getPwd(), "");
     }
 }
